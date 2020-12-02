b81c40a453de43128b3a0fd410998f894b25f755
Trevbytes <trevor.j.adams@outlook.com>
2020-12-02 13:15:00 +0000

    Add simple model for newsletter

--- djangonewsletter/newsletter/models.py
+++ djangonewsletter/newsletter/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class NewsletterSignup(models.Model):
+
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    postcode = models.CharField(max_length=20, null=False, blank=False)
+
+    def __str__(self):
+        """Return the request/order number."""
+        return self.email
