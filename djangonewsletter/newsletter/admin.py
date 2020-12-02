be59c7bce736d8a5d1af52e3d852e900c06260c2
Trevbytes <trevor.j.adams@outlook.com>
2020-12-02 13:14:26 +0000

    Add Admin model with code to export Csv

--- djangonewsletter/newsletter/admin.py
+++ djangonewsletter/newsletter/admin.py
@@ -1,3 +1,37 @@
+import csv
 from django.contrib import admin
+from .models import NewsletterSignup
+from django.http import HttpResponse
 
-# Register your models here.
+
+class ExportCsvMixin:
+    def export_as_csv(self, request, queryset):
+
+        meta = self.model._meta
+        field_names = [field.name for field in meta.fields]
+
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename={}.csv'.format(meta)
+        writer = csv.writer(response)
+
+        writer.writerow(field_names)
+        for obj in queryset:
+            row = writer.writerow([getattr(obj, field) for field in field_names])
+
+        return response
+
+    export_as_csv.short_description = "Export Selected"
+
+
+class NewsletterAdmin(admin.ModelAdmin, ExportCsvMixin):
+
+    fields = ('full_name', 'email', 'postcode',)
+
+    list_display = ('full_name', 'email', 'postcode',)
+
+    ordering = ('-full_name',)
+
+    actions = ["export_as_csv"]
+
+
+admin.site.register(NewsletterSignup, NewsletterAdmin)
